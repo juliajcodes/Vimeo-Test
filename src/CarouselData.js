d39f035d0e27b44b669cd96ba23211369d767577
juliajohnson18 <32076859+juliajohnson18@users.noreply.github.com>
2019-10-23 16:36:14 -0400

    Added Data for Carousel

---
+++ src/CarouselData.js
@@ -0,0 +1,50 @@
+const data = {
+  movies: [
+    {
+      index: 0,
+      title: "Hunt for the Wilderpeople",
+      description:
+        "Raised on hip-hop and foster care, defiant city kid Ricky gets a fresh start in the New Zealand countryside. From the director of What We do in The Shadows.",
+      filmImage:
+        "https://cdn.traileraddict.com/content/the-orchard/hunt-for-the-wilderpeople-poster-6.jpg",
+      link: "https://vimeo.com/ondemand/huntforthewilderpeople"
+    },
+    {
+      index: 1,
+      title: "Nichts passiert / A Decent Man",
+      description:
+        "A Swiss Family takes a ski vacation into trouble when the father, a titular decent man, finds himself in a series of moral quandaries.",
+      filmImage:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSBKHT7bG3k_qsXRf6Xv8giuIY5r_vI6gjepJoW2zeN5CjwJYSG",
+      link: "https://vimeo.com/155591000"
+    },
+    {
+      index: 2,
+      title: "Vice Versa",
+      description:
+        "Come along with the good company crew as they travel throughout the US, Japan, BC, and Quebec to showcase skiing in the best way possible.",
+      filmImage: "http://skimoviemusic.net/images/VV-GC.jpg",
+      link: "https://vimeo.com/ondemand/viceversafilm/178074729"
+    },
+    {
+      index: 3,
+      title: "The Fourth Phase",
+      description:
+        "From the creators of The Art of FLIGHT, Red Bull Media House presents THE FOURTH PHASE, a snowboarding epic starring iconic athlete Travis Rice",
+      filmImage:
+        "https://m.media-amazon.com/images/M/MV5BOGVhNjUwYTItYmI2NS00ZGI1LWE5ZDQtMWVjMTIyODAzMDI4XkEyXkFqcGdeQXVyMjQ3NzUxOTM@._V1_.jpg",
+      link: "https://vimeo.com/ondemand/thefourthphase"
+    },
+    {
+      index: 4,
+      title: "Full Moon",
+      description:
+        "Be inspired by women who push boundaries. FULL MOON showcases the legends, current icons and future prodigies of this ever-evolving lifestyle sport.",
+      filmImage:
+        "https://is3-ssl.mzstatic.com/image/thumb/Video62/v4/b5/f9/42/b5f942c6-1ab9-dd18-50eb-c6e71aab3441/pr_source.lsr/268x0w.png",
+      link: "https://vimeo.com/ondemand/fullmoonsnowboard"
+    }
+  ]
+};
+
+export default data;
